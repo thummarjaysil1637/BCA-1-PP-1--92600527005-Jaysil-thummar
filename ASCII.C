f01c6b47ab84e2417ffb9bee786f10de48c96a4f
thummarjaysil1637 <thummarjaysil1637@gmail.com>
2026-08-14 11:03:21 +0530

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	char x;
+	clrscr();
+	printf("\n Enter Any Character : ");
+	scanf("%c",&x);
+
+	printf("\n Input Character is : %c" ,x+32);
+	printf("\n ASCII code is : %d" ,x+32);
+	getch();
+}
